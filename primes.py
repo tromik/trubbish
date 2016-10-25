9428e7f028f728c8d63bdf3c795cac2f4c4b1fbe
Tom Ross <tromik@gmail.com>
2016-10-25 16:20:46 -0400

    Count 1000th prime

---
+++ primes.py
@@ -0,0 +1,28 @@
+
+i = 1
+n = 10000
+
+def findPrime(i, n):
+    # count starts at 1 because 2 is the first prime
+    count = 1
+    while i < n:
+        i = i + 2 # Generate odd numbers greater than 1
+        j = 1 # Start j at 1 as prime can be divisible by 1 and itself
+        prime = True
+        while j < n:
+            j = j + 1
+            # Do not count if the candidate is the same as the divisor
+            if i != j:
+                # if the remainder is 0 candidate is not a prime and can exit check
+                if i % j == 0:
+                    prime = False
+                    break
+        # If the candidate comes out of the loop still prime increment the counter
+        if prime is True:
+            count = count + 1
+            if count == 1000:
+                return i
+                break
+
+
+print str(findPrime(i, n)) + ' is the 1000th prime'
