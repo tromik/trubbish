0ee722b18cd1943805b90c135859aa176fb26f21
Tom Ross <tromik@gmail.com>
2016-10-26 12:11:18 -0400

    Updated primes.py with prime loop, completed primes_log.py

--- prime_log.py
+++ prime_log.py
@@ -1,27 +1,34 @@
 import math
 
 i = 1
-n = 10000
+n = 250000
+fnd = 25000
 
-def findPrime(i, n):
+def findPrime(i, n, fnd):
     # count starts at 1 because 2 is the first prime
-    logsum = 0
     count = 1
+    primes = []
+    logsum = 0
     while i < n:
         i = i + 2 # Generate odd numbers greater than 1
         j = 1 # Start j at 1 as prime can be divisible by 1 and itself
         prime = True
-        while j < n:
-            j = j + 1
+        for j in primes: # Only need to loop through primes less than the candidate
             # if the remainder is 0 candidate is not a prime and can exit check
             if i != j and i % j == 0:
                 prime = False
                 break
         # If the candidate comes out of the loop still prime increment the counter
-        if prime is True:
-            logsum = logsum + math.exp(i)
-        print 'Sum: ' + str(logsum) + ' | n: ' + str(n) + ' | Ratio: ' + str(logsum/n)
+        if prime:
+            count = count + 1 # Increment prime counter
+            primes.append(i) # Add prime to list of primes for next loop
+            logsum = logsum + math.log(i)
+            print 'Log sum: ' + str(logsum) + ' | Current prime: ' + str(i) + ' | Ratio: ' + str(logsum/n)
+            if count == fnd:
+                return i
+                break
 
-
-# print str(findPrime(i, n)) + ' is the 1000th prime'
-findPrime(i, n)
+if not findPrime(i, n, fnd):
+    print 'Could not reach the ' + str(fnd) + 'th prime with maximum limit of ' + str(n)
+else:
+    print str(findPrime(i, n)) + ' is the ' + str(fnd) + 'th prime'
