a9f56264b21fbde67cc3f034f30f8cbdae06f184
Tom Ross <tromik@gmail.com>
2016-10-29 09:01:39 -0400

    Made program more efficient

--- deq.py
+++ deq.py
@@ -1,7 +1,18 @@
+minv = 0
+maxv = 501
 
-for i in range(1, 60):
-    for x in range(0, 100):
-        for y in range(0, 100):
-            for z in range(0, 100):
-                if 6*x + 9*y + 20*z == i:
+def nuggets(minv, maxv):
+    # import pdb; pdb.set_trace()
+    for i in range(minv, maxv):
+        z = 0
+        while 20*z < maxv:
+            y = 0
+            while 9*y < maxv:
+                x = (i - 9*y - 20*z)/6.0
+                if x.is_integer() and x >= 0:
+                    x = int(x)
                     print "Test is true for " + str(i) + " x = " + str(x) + " y = " + str(y) + " z = " + str(z)
+                y = y + 1
+            z = z + 1
+
+nuggets(minv, maxv)
