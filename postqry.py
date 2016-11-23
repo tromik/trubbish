3aaf149b0b70313431ccf8b0dbd489c9fd728129
Tom Ross <tromik@gmail.com>
2016-11-22 21:10:26 -0500

    Added function for select

--- postqry.py
+++ postqry.py
@@ -30,81 +30,83 @@ except psycopg2.OperationalError as e:
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 booking_doft_qry = '2016-11-21'
+def insert_accrual(booking_doft_qry='1900-01-01'):
+    sel_query = """select cast('Hotel' as varchar(50)) as type,
+                    s."ServiceID" as service_id,
+                    s."BookingNo" as booking_no,
+                    b."Date_of_First_Travel" as date_of_first_travel,
+                    s."Service_Start_Date" as service_start_date,
+                    cast(s."Vendor_Code" as varchar(35)) as vendor_code,
+                    cast(case when (s."Cost_Currency" = 'ZAR') then 'ZAR' else 'USD' end as varchar(5)) as currency,
+                    s."Paid_By" as paid_by,
+                    case when (s."Cost_Currency" = 'ZAR') then s."Validated_Cost_Local" else s."Validated_Cost_USD" end as amount,
+                    s."EntityID" as entity_id,
+                    m.markup,
+                    gl.gl_account_name,
+                    gl.gl_account_number,
+                    gl.company,
+                    cast(gl.revenue_stream as varchar(100)) as revenue_stream
+                    from service s
+                    Left Join booking b ON s."BookingNo" = b."BookingNo"
+                    Left Join smart_project_entity e
+                    on coalesce(s."EntityID", 1) = e.id
+                    Left Join smart_project_servicetypemarkup m
+                    on e.id = m.entity_id
+                    and m.service_type = 'Pre Hotel'
+                    Join tour_cost_gl_mappings_africa gl
+                    on gl.revenue_stream = 'Accomodation'
+                    and (gl.gl_account_name = 'Accommodation'
+                    or gl.gl_account_name = 'Accrued Liabilities')
+                    Where s."Service_Type" = 'Hotel'
+                    AND s."Service_Status" = 'Confirmed'
+                    AND s."Paid_By" IN ('D-LO','Ldr','D-TO')
+                    AND (s."Expected_Cost_Currency" is not null OR s."Validated_Cost_Currency" is not null)
+                    AND (s."Expected_Cost_Local" > 0 OR s."Validated_Cost_Local" > 0)
+                    AND char_length(s."Invoice_Number") = 0
+                    AND b."Date_of_First_Travel" = current_date - interval '2 days'
+                    AND coalesce(s."EntityID") = 2
 
-sel_query = """select cast('Hotel' as varchar(50)) as type,
-                s."ServiceID" as service_id,
-                s."BookingNo" as booking_no,
-                b."Date_of_First_Travel" as date_of_first_travel,
-                s."Service_Start_Date" as service_start_date,
-                cast(s."Vendor_Code" as varchar(35)) as vendor_code,
-                cast(case when (s."Cost_Currency" = 'ZAR') then 'ZAR' else 'USD' end as varchar(5)) as currency,
-                s."Paid_By" as paid_by,
-                case when (s."Cost_Currency" = 'ZAR') then s."Validated_Cost_Local" else s."Validated_Cost_USD" end as amount,
-                s."EntityID" as entity_id,
-                m.markup,
-                gl.gl_account_name,
-                gl.gl_account_number,
-                gl.company,
-                cast(gl.revenue_stream as varchar(100)) as revenue_stream
-                from service s
-                Left Join booking b ON s."BookingNo" = b."BookingNo"
-                Left Join smart_project_entity e
-                on coalesce(s."EntityID", 1) = e.id
-                Left Join smart_project_servicetypemarkup m
-                on e.id = m.entity_id
-                and m.service_type = 'Pre Hotel'
-                Join tour_cost_gl_mappings_africa gl
-                on gl.revenue_stream = 'Accomodation'
-                and (gl.gl_account_name = 'Accommodation'
-                or gl.gl_account_name = 'Accrued Liabilities')
-                Where s."Service_Type" = 'Hotel'
-                AND s."Service_Status" = 'Confirmed'
-                AND s."Paid_By" IN ('D-LO','Ldr','D-TO')
-                AND (s."Expected_Cost_Currency" is not null OR s."Validated_Cost_Currency" is not null)
-                AND (s."Expected_Cost_Local" > 0 OR s."Validated_Cost_Local" > 0)
-                AND char_length(s."Invoice_Number") = 0
-                AND b."Date_of_First_Travel" = current_date - interval '2 days'
-                AND coalesce(s."EntityID") = 2
+                    UNION ALL
 
-                UNION ALL
+                    select
+                    cast('Transfer' as varchar(50)) as type,
+                    s."ServiceID" as service_id,
+                    s."BookingNo" as booking_no,
+                    b."Date_of_First_Travel" as date_of_first_travel,
+                    s."Service_Start_Date" as service_start_date,
+                    cast(s."Vendor_Code" as varchar(35)) as vendor_code,
+                    cast(case when (s."Cost_Currency" = 'ZAR') then 'ZAR' else 'USD' end as varchar(5)) as currency,
+                    s."Paid_By" as paid_by,
+                    case when (s."Cost_Currency" = 'ZAR') then s."Validated_Cost_Local" else s."Validated_Cost_USD" end as amount,
+                    s."EntityID" as entity_id,
+                    m.markup,
+                    gl.gl_account_name,
+                    gl.gl_account_number,
+                    gl.company,
+                    cast(gl.revenue_stream as varchar(100)) as revenue_stream
+                    from service s
+                    Left Join booking b ON s."BookingNo" = b."BookingNo"
+                    Left Join smart_project_entity e
+                    on coalesce(s."EntityID", 1) = e.id
+                    Left Join smart_project_servicetypemarkup m
+                    on e.id = m.entity_id
+                    and m.service_type = 'Pre Hotel'
+                    Join tour_cost_gl_mappings_africa gl
+                    on gl.revenue_stream = 'Transfer'
+                    and (gl.gl_account_name = 'Transfer'
+                    or gl.gl_account_name = 'Accrued Liabilities')
+                    Where s."Service_Type" = 'Transfer'
+                    AND s."Service_Status" = 'Confirmed'
+                    AND s."Paid_By" IN ('D-LO','Ldr','D-TO')
+                    AND (s."Expected_Cost_Currency" is not null OR s."Validated_Cost_Currency" is not null)
+                    AND (s."Expected_Cost_Local" > 0 OR s."Validated_Cost_Local" > 0)
+                    AND char_length(s."Invoice_Number") = 0
+                    --AND b."Date_of_First_Travel" = current_date - interval '2 days'
+                    AND b."Date_of_First_Travel" = '""" + booking_doft_qry + """'
+                    AND coalesce(s."EntityID") = 2
+                    Order By type, service_id, gl_account_name"""
 
-                select
-                cast('Transfer' as varchar(50)) as type,
-                s."ServiceID" as service_id,
-                s."BookingNo" as booking_no,
-                b."Date_of_First_Travel" as date_of_first_travel,
-                s."Service_Start_Date" as service_start_date,
-                cast(s."Vendor_Code" as varchar(35)) as vendor_code,
-                cast(case when (s."Cost_Currency" = 'ZAR') then 'ZAR' else 'USD' end as varchar(5)) as currency,
-                s."Paid_By" as paid_by,
-                case when (s."Cost_Currency" = 'ZAR') then s."Validated_Cost_Local" else s."Validated_Cost_USD" end as amount,
-                s."EntityID" as entity_id,
-                m.markup,
-                gl.gl_account_name,
-                gl.gl_account_number,
-                gl.company,
-                cast(gl.revenue_stream as varchar(100)) as revenue_stream
-                from service s
-                Left Join booking b ON s."BookingNo" = b."BookingNo"
-                Left Join smart_project_entity e
-                on coalesce(s."EntityID", 1) = e.id
-                Left Join smart_project_servicetypemarkup m
-                on e.id = m.entity_id
-                and m.service_type = 'Pre Hotel'
-                Join tour_cost_gl_mappings_africa gl
-                on gl.revenue_stream = 'Transfer'
-                and (gl.gl_account_name = 'Transfer'
-                or gl.gl_account_name = 'Accrued Liabilities')
-                Where s."Service_Type" = 'Transfer'
-                AND s."Service_Status" = 'Confirmed'
-                AND s."Paid_By" IN ('D-LO','Ldr','D-TO')
-                AND (s."Expected_Cost_Currency" is not null OR s."Validated_Cost_Currency" is not null)
-                AND (s."Expected_Cost_Local" > 0 OR s."Validated_Cost_Local" > 0)
-                AND char_length(s."Invoice_Number") = 0
-                --AND b."Date_of_First_Travel" = current_date - interval '2 days'
-                AND b."Date_of_First_Travel" = '""" + booking_doft_qry + """'
-                AND coalesce(s."EntityID") = 2
-                Order By type, service_id, gl_account_name"""
+    return sel_query
 
 try:
     cur.execute(sel_query)
