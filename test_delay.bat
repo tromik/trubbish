b147468f801f271dce3521138ccc5332fee31d40
Tom Ross <tromik@gmail.com>
2016-12-25 11:41:38 -0500

    Testing delay commands

---
+++ test_delay.bat
@@ -0,0 +1,5 @@
+@echo "Staring at %DATE:/=-%@%TIME::=-%-----------------------"
+sleep 10
+@echo "Post-sleep for 10 %DATE:/=-%@%TIME::=-%----------------"
+timeout /t 10
+@echo "Post-timeout for 10 %DATE:/=-%@%TIME::=-%--------------"
